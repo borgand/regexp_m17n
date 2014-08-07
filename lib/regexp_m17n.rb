02bee8ef022dbfda81ab7b257fab1da4b5cc534e
Laas Toom <laas.toom@gmail.com>
2014-08-07 13:55:15 +0300

    Incl some dummies, skipping only those that do not have converter

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,11 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ Regexp.new('^.+$'.encode(str.encoding))
+    if !str.encoding.dummy?
+      # Try to preserve original string and make regexp in corresponding encoding
+      str =~ Regexp.new('^.+$'.encode(str.encoding))
+    else
+      # We can't make Regexp out of dummy encoding so try matching in UTF-8 instead
+      str.encode('UTF-8') =~ Regexp.new('^.+$'.encode('UTF-8'), Regexp::FIXEDENCODING)
+    end
   end
 end
