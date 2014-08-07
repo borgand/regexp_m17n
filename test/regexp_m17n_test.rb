ce5d3e9bef3e7179f23a49c0602ce74dc6017027
Laas Toom <laas.toom@gmail.com>
2014-08-07 14:07:40 +0300

    Rejected some ISO-2022 encodings too eagerly based on name

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -6,7 +6,7 @@ class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
       # Skip encodings that Ruby has no converter for UTF-8
-      next if %w[ISO-2022-JP ISO-2022-JP-2 CP50220 CP50221 ISO-2022-JP-KDDI UTF-7].include?(enc.name)
+      next if %w[ISO-2022-JP-2 UTF-7].include?(enc.name)
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
